e52d8715f80ead5ddca425ff798e708ee10a9b0b
unknown <thompson.jonm@gmail.com>
2017-09-15 15:38:41 -0500

    server test op

---
+++ testing/server.test.js
@@ -0,0 +1,23 @@
+// Dependencies
+var expect = require('chai').expect;
+var request = require('supertest');
+
+var server = require('./../server');
+
+describe('listen', function() {
+    var server;
+
+    beforeEach(function() {
+        server = require('./../server');
+    });
+
+    afterEach(function() {
+        server.close();
+    });
+
+    it('responds to /', function testSlash(done) {
+        request(server)
+            .get('/')
+            .expect(200, done);
+    });
+});
