14197abe16d5846a21f1ec150868b526c400813f
unknown <thompson.jonm@gmail.com>
2017-09-18 14:59:26 -0500

    fixed validation error with headline length

---
+++ models/dataModel.js
@@ -0,0 +1,37 @@
+// Model goes here
+
+//Dependencies
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var WebScrapeSchema = Schema ({
+    headline: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [
+            function(input) {
+                return input.length >= 5;
+            },
+        'Headlines must be longer than five characters.'
+        ]
+    },
+
+    synopsis: {
+        type: String,
+        required: true
+    },
+
+    articleURL: {
+        type: String,
+        required: true,
+        match: [
+            /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/, 
+            'Please enter a valid URL'
+        ]
+    }
+});
+
+var WebScrapes = mongoose.model('WebScrapes', WebScrapeSchema);
+
+module.exports = WebScrapes;
